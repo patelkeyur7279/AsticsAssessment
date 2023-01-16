f2c02d97a75edd78489e3d32753bf163b19070ea
Keyur Patel <patelkeyur7279@gmail.com>
2023-01-16 13:57:51 +0530

    Initial Commit

---
+++ AsticsAssessment/AsticsAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsticsAssessmentApp.swift
+//  AsticsAssessment
+//
+//  Created by Keyur Patel on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AsticsAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
